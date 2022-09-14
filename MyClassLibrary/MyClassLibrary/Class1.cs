aca90f67aac9717bf264eb0d9c75d914f64be6d8
horneddragon <73488864+horneddragon@users.noreply.github.com>
2022-09-14 18:45:46 +0530

    Add files via upload

---
+++ MyClassLibrary/MyClassLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace MyClassLibrary
+{
+    public class Class1
+    {
+        internal interface ICarPrice
+        {
+            int GetPrice(string name);
+        }
+
+        class BMW : ICarPrice
+        {
+            public int GetPrice(string name)
+            {
+                if (name == "M3 GTR")
+                {
+                    return 5000000;
+                }
+                else if (name == "X7")
+                {
+                    return 90000000;
+                }
+                else
+                {
+                    return 1000000;
+                }
+            }
+
+        }
+
+    }
+}
