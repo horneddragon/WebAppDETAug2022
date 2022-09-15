f4f216eab6c6a92a39778b499d9fe615c1055f91
Anirudha <anirudhapuranic@outlook.com>
2022-09-15 16:56:36 +0530

    n

---
+++ TTDDemo/Rooter.cs
@@ -0,0 +1,17 @@
+﻿namespace TTDDemo
+{
+    internal class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            double result = input;
+            double previousResult = -input;
+            while (Math.Abs(previousResult - result) > result / 1000)
+            {
+                previousResult = result;
+                result = (result + input / result) / 2;
+            }
+            return result;
+        }
+    }
+}
